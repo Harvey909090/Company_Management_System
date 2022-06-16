9a01ccbbd56a586b033b93d3170a57c4c129e3f7
George <chilverth@gmail.com>
2022-06-17 02:35:34 +0300

    Exception handling

---
+++ src/main/java/Company_System/dao/util/BuildInsertStatementException.java
@@ -0,0 +1,11 @@
+package Company_System.dao.util;
+
+public class BuildInsertStatementException extends Exception {
+
+    public BuildInsertStatementException() {
+    }
+
+    public BuildInsertStatementException(String msg) {
+        super(msg);
+    }
+}
